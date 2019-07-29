93dedac80b493b72666a372dc1209eae48df8d12
Felipe Almeida <felipealmeida@Felipes-MacBook-Pro.local>
2019-07-29 18:40:39 -0300

    Final Commit

--- WeatherMiniChallenge.py
+++ WeatherMiniChallenge.py
@@ -1,10 +1,99 @@
 #!/usr/bin/python
+# -*- coding: UTF-8 -*-
+################################################################################
+# Module:   WeatherMiniChallenge.py     Autor: Felipe Almeida                  #
+# Start:    29-Jul-2019                 Last Update: 29-Jul-2019  Version: 1.0 #
+################################################################################
 
 import sys
-import os
+import requests
+import json
+from datetime import datetime, timezone
+import calendar
+
+aaa = "https://openweathermap.org/city/6322515"
+
+CountryISOCode  = 'BR'
+OWM_ApiKey      = '11111111111111111111111111111111'
+OWM_EndPoint    = 'api.openweathermap.org'
+OWM_Version     = '2.5'
+OWM_GlobalUrl   = 'https://'+OWM_EndPoint+'/data/'+OWM_Version+'/'
+
+def GetWeatherForecast(v_CityId = -1):
+    global CountryISOCode, OWM_ApiKey, OWM_GlobalUrl
+
+    StrReturn   = ''
+    PrevWeekDay = ''
+
+    if v_CityId == -1:
+        return -1
+    else:
+        Url = OWM_GlobalUrl + 'forecast'
+        QueryString = {
+            'APPID': OWM_ApiKey,
+            'id': v_CityId,
+            'cnt': 40 # 5 days, 3 Hours Interval
+        }
+        Headers = {
+            'User-Agent': "Mozilla/5.0",
+            'Accept': "*/*",
+            'Cache-Control': "no-cache",
+            'Host': "openweathermap.org",
+            'accept-encoding': "gzip, deflate",
+            'Connection': "keep-alive",
+            'cache-control': "no-cache"
+        }
+        Response = requests.request("GET",Url, headers=Headers, params=QueryString)
+        jResponse = json.loads(Response.text)
+        for jDay in jResponse['list']:
+            UnixTSDate  = jDay['dt']
+            Humidity    = jDay['main']['humidity']
+            dtDataDate  = datetime.fromtimestamp(UnixTSDate, timezone.utc)
+            StrWeekDay  = calendar.day_name[dtDataDate.weekday()]
+            StrDataHour = dtDataDate.strftime("%H")
+
+            if (Humidity > 70):
+                if (StrWeekDay == PrevWeekDay):
+                    StrReturn = StrReturn+StrDataHour+','
+                else:
+                    StrReturn = StrReturn[:-1]+'), '+StrWeekDay+' @('+StrDataHour+','
+                PrevWeekDay = StrWeekDay
+
+    StrReturn = StrReturn[2:-1].strip()+')'
+    print('You should take an umbrella in these days/hours: '+StrReturn)
+
+def GetCityId(v_CityName = 'Ribeirão Preto'):
+    global CountryISOCode, OWM_ApiKey, OWM_GlobalUrl
+
+    CityCode = 0
+    Url = OWM_GlobalUrl + 'weather'
+    QueryString = {
+        'APPID': OWM_ApiKey,
+        'q': v_CityName+','+CountryISOCode.lower()
+    }
+    Headers = {
+        'User-Agent': "Mozilla/5.0",
+        'Accept': "*/*",
+        'Cache-Control': "no-cache",
+        'Host': "openweathermap.org",
+        'accept-encoding': "gzip, deflate",
+        'Connection': "keep-alive",
+        'cache-control': "no-cache"
+    }
+    Response = requests.request("GET",Url, headers=Headers, params=QueryString)
+    jResponse = json.loads(Response.text)
+    try:
+        if v_CityName == jResponse['name']:
+            CityCode = jResponse['id']
+    except:
+        CityCode = -1
+
+    return CityCode
 
 def MainProcess():
-    print("Weather Mini Challenge")
+    if (GetWeatherForecast(GetCityId()) == -1):
+        print('Invalid City Name')
+        sys.exit(1)
 
 def main():
     try:
